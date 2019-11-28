de3f9481f0a1e9b4e39cfcd55fde7ac6bfcfd536
Josias <kologojosias@gmail.com>
2019-11-28 16:32:39 +0100

    Updates

---
+++ precache-manifest.783c4b75022d5ac4cd6d56026a10aac6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "469ec7e0bf839cca88475e21f133cbc2",
+    "url": "/typing-speed-test/index.html"
+  },
+  {
+    "revision": "0554c9ffd0a67e9bdcae",
+    "url": "/typing-speed-test/static/css/main.d674e081.chunk.css"
+  },
+  {
+    "revision": "f758c50a56b4e6ed9b99",
+    "url": "/typing-speed-test/static/js/2.6b4749e0.chunk.js"
+  },
+  {
+    "revision": "0554c9ffd0a67e9bdcae",
+    "url": "/typing-speed-test/static/js/main.270dde24.chunk.js"
+  },
+  {
+    "revision": "392ec0a0b17165855dfc",
+    "url": "/typing-speed-test/static/js/runtime-main.779116fe.js"
+  }
+]);
